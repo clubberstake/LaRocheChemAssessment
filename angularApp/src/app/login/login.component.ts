893b649255d9e4b47ea29a325050fcfa935438f6
PontiacBrad <bradley.gray@stu.laroche.edu>
2018-04-10 20:45:49 -0400

    Added Md5 hashing for the password

--- angularApp/src/app/login/login.component.ts
+++ angularApp/src/app/login/login.component.ts
@@ -2,17 +2,19 @@ import { Component, Input, OnInit } from '@angular/core';
 import { userObject } from '../userObject';
 import { UserService } from '../services/user-service.service'
 import { UserInfo } from '../login/userInfo'
+import { Md5 } from 'ts-md5/dist/md5'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [Md5]
 })
 export class LoginComponent implements OnInit {
   @Input() userObject: userObject;
   username: String = "";
   emailFeedback: String = "";
-  password: String = "";
+  password: string = "";
   users: UserInfo[];
 
   constructor(private userService: UserService) { }
@@ -24,6 +26,7 @@ export class LoginComponent implements OnInit {
   }
 
   logIn() {
+    var hashedpassword = Md5.hashStr(this.password);
     this.userObject.user = this.users.find(
       value => value.username == this.username
     );
@@ -33,7 +36,7 @@ export class LoginComponent implements OnInit {
     }
     else
     {
-      if(this.userObject.user.username == this.username && this.userObject.user.password == this.password)
+      if(this.userObject.user.username == this.username && this.userObject.user.password == hashedpassword)
       {
         this.userObject.loggedIn = true;
       }
--- restfulApi/src/main/java/laroche/chem/assessment/restApp/UserController.java
+++ restfulApi/src/main/java/laroche/chem/assessment/restApp/UserController.java
@@ -27,12 +27,18 @@ public class UserController {
 		
 		List<Users> users = userRepository.findAll();
 		if (!users.iterator().hasNext()) {
-			userRepository.save(new Users("jon.nichols@stu.laroche.edu", "Superdude", "student"));
-			userRepository.save(new Users("bradley.gray@laroche.edu", "Lamborghiniman", "admin"));
-			userRepository.save(new Users("greg.lang@laroche.edu", "teacher5", "teacher"));
-			userRepository.save(new Users("reggie.fill@stu.laroche.edu", "regginator", "student"));
-			userRepository.save(new Users("steve.jobs@laroche.edu", "iPassword", "teacher"));
-			userRepository.save(new Users("bill.gates@laroche.edu", "macdaddy", "admin"));
+			userRepository.save(new Users("jon.nichols@stu.laroche.edu", "bdd499ffb20d25b636d22f915a9f895a", "student"));
+            //pswd is Superdude for jon
+            userRepository.save(new Users("bradley.gray@laroche.edu", "bfbb9026b6f039f9e9dfb46f5c9c1aa9", "admin"));
+            //pswd is Lamborghiniman for bradley
+            userRepository.save(new Users("greg.lang@laroche.edu", "ea105f0d381e790cdadc6a41eb611c77", "teacher"));
+            //pswd is teacher5 for greg
+            userRepository.save(new Users("reggie.fill@stu.laroche.edu", "3c1bada5012fa1d2f708c39228e8f033", "student"));
+            //pswd is regginator for reggie
+            userRepository.save(new Users("steve.jobs@laroche.edu", "b54619aaeaae0e6078f4df37462b4cf4", "teacher"));
+            //pswd is iPassword for steve
+            userRepository.save(new Users("bill.gates@laroche.edu", "5ea87fe90e14926b50ddbee77c889d1c", "admin"));
+            //pswd is macdaddy for bill
 			users = userRepository.findAll();
 		}
 		
