b438f3d0ca5eb971838fab9641af41277a4815cf
abdullah4550 <abdullah.almusallam@stu.laroche.edu>
2018-04-25 00:31:25 -0400

    Added unit test for StudentInfoForBioAndAdmissionsPlacementTabService

--- angularApp/src/app/services/student-info-for-bio-and-admissions-placement-tab.service.spec.ts
+++ angularApp/src/app/services/student-info-for-bio-and-admissions-placement-tab.service.spec.ts
@@ -1,15 +1,70 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { StudentInfoForBioAndAdmissionsPlacementTabService } from './student-info-for-bio-and-admissions-placement-tab.service';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+import { StudentInfoForBioAndAdmissionsPlacementTabResponse } from '../individual-learning-record/studentInfoForBioAndAdmissionsPlacementTabResponse';
+import { FileStorage } from './file-storage';
 
-describe('StudentInfoForBioAndAdmissionsPlacementTabService', () => {
+fdescribe('When creating the studentInfo service ', () => {
+let service: StudentInfoForBioAndAdmissionsPlacementTabService; // Initializing my service
+let httpMock: HttpTestingController;  // Initializing my mock http
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [StudentInfoForBioAndAdmissionsPlacementTabService]
+      imports: [HttpClientTestingModule],  // Importing the testing module
+      providers: [StudentInfoForBioAndAdmissionsPlacementTabService] // Importing the Service 
     });
+    service = TestBed.get(StudentInfoForBioAndAdmissionsPlacementTabService);  // Instantiating the service and http mock
+    httpMock = TestBed.get(HttpTestingController);
   });
 
-  it('should be created', inject([StudentInfoForBioAndAdmissionsPlacementTabService], (service: StudentInfoForBioAndAdmissionsPlacementTabService) => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify(); // Verifying the http mock here
+  })
+
+  it('should return a proper student Information ', inject([StudentInfoForBioAndAdmissionsPlacementTabService], (service: StudentInfoForBioAndAdmissionsPlacementTabService) => {
+    const dummyStudentInfos: StudentInfoForBioAndAdmissionsPlacementTabResponse[]  = 
+    [
+      {
+       id: 1,
+       file: new FileStorage(0, "", ""),
+       studentName: "",
+       studentMajor: "",
+       studentYear: "",
+       studentSemester: "",
+       studentMathGrade:"",
+       studentAthletics: "",
+       studentHousingStatus: "",
+       studentHonors: "",
+       internationalStudent: "",
+       time: "",
+      }
+    ];
+
+    const dummyStudentInfo: StudentInfoForBioAndAdmissionsPlacementTabResponse = 
+    { 
+      id: 1,
+      file: new FileStorage(0, "", ""),
+      studentName: "",
+      studentMajor: "",
+      studentYear: "",
+      studentSemester: "",
+      studentMathGrade:"",
+      studentAthletics: "",
+      studentHousingStatus: "",
+      studentHonors: "",
+      internationalStudent: "",
+      time: "",
+    }
+
+
+    service.getStudentInfo().subscribe(studentInfos => {
+      expect(studentInfos.length).toBeGreaterThan(0);
+      expect(studentInfos).toEqual(dummyStudentInfos);
+    });
+
+    const getStudentInfo = httpMock.expectOne("http://localhost:8080/student/studentInfoForBioAndAdmissionsPlacementTab");
+    expect(getStudentInfo.request.method).toBe("GET");
+    getStudentInfo.flush(dummyStudentInfos);
+
   }));
 });
