bb0056b738f10eb71e46da6329ecd866b50ac74b
j-nicholson <johnfnicholson123@gmail.com>
2018-05-01 17:47:31 -0400

    Fixed some merge conflicts and services based on new Student object field.

--- angularApp/src/app/individual-learning-record/student-pane/student-pane.component.ts
+++ angularApp/src/app/individual-learning-record/student-pane/student-pane.component.ts
@@ -20,7 +20,7 @@ export class StudentPaneComponent implements OnInit {
   time = new Date(); // Initializing Time
   month: number; // Decalring the month type
   currentdate = this.time.getUTCMonth() + '/' + this.time.getUTCDate() + '/' + this.time.getUTCFullYear()
- 
+
   majors = ["Chemistry", "Biochemistry", "Other"];
   years = ["Sophomore", "Junior", "Senior"];
   semesters = ["Fall", "Spring", "Summer"];
@@ -31,7 +31,7 @@ export class StudentPaneComponent implements OnInit {
     private fileStorageService: FileStorageService  ) {}
 
   ngOnInit() {
-    this.studentObjectInput.student.file = new FileStorage(0,'','');
+    this.studentObjectInput.student.file = new FileStorage(0,'','','');
   }
 
   onFileSelected(event) {
@@ -65,18 +65,18 @@ export class StudentPaneComponent implements OnInit {
       });
     }
     else{
-      this.studentObjectInput.student.file = new FileStorage(0,'','');      
+      this.studentObjectInput.student.file = new FileStorage(0,'','','');
     }
 
     this.studentObjectInput.student.id = this.studentObjectInput.studentId;
     console.log(this.studentObjectInput);
     this.studentService.updateStudent(this.studentObjectInput.student);
-    // Swal({
-    //   type: 'success',
-    //   title: 'Student Updated!',
-    //   showConfirmButton: false,
-    //   timer: 1500
-    // })
+    Swal({
+      type: 'success',
+      title: 'Student Updated!',
+      showConfirmButton: false,
+      timer: 1500
+    })
   }
 
   private sleep(ms) {
